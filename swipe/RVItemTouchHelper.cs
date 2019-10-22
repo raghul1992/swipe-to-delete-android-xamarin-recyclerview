634d04437ee04d49fa19edc2a5335e130a3848aa
Raul <raghulgk@gmail.com>
2019-10-23 03:34:50 +0530

    Update RVItemTouchHelper.cs

--- swipe/RVItemTouchHelper.cs
+++ swipe/RVItemTouchHelper.cs
@@ -1,80 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.Graphics;
-using Android.OS;
-using Android.Runtime;
-using Android.Support.V7.Widget;
-using Android.Support.V7.Widget.Helper;
-using Android.Views;
-using Android.Widget;
-
-namespace swipe
-{
-    class RVItemTouchHelper : ItemTouchHelper.SimpleCallback
-    {
+<?xml version="1.0" encoding="utf-8"?>
+<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content">
 
 
-        private RVItemTouchHelperListener _listener;
+     <RelativeLayout
+                android:background="@android:color/black"
 
-        public RVItemTouchHelper(int dragDirs, int swipeDirs, RVItemTouchHelperListener listener) : base(dragDirs, swipeDirs)
-        {
+           android:id="@+id/bg_left" 
+        android:layout_width="match_parent"
+        android:layout_height="match_parent">
 
-            _listener = listener;
-        }
+          <ImageView
+       android:layout_width="wrap_content"
+       android:layout_height="wrap_content"
+   android:layout_alignParentLeft="true"
+        android:src="@drawable/delete"
+            android:tint="@android:color/white"
+        />
 
-        public override bool OnMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target)
-        {
-            return false;
-        
-        }
-
-        public override void OnSwiped(RecyclerView.ViewHolder viewHolder, int direction)
-        {
-
-            if (_listener != null) {
-                _listener.onSwiped(viewHolder,direction,viewHolder.AdapterPosition);
-            }
-
-        }
-
-
-        public override void ClearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder)
-        {
-            View fg = ((recyclarAdapter.ViewHolder)viewHolder).fg;
-            DefaultUIUtil.ClearView(fg);  
-        }
-
-        public override void OnSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState)
-        {
-            if (viewHolder != null) {
-                View fg = ((recyclarAdapter.ViewHolder)viewHolder).fg;
-                DefaultUIUtil.OnSelected(fg);
-            }
-        }
-        public override int ConvertToAbsoluteDirection(int flags, int layoutDirection)
-        {
-            return base.ConvertToAbsoluteDirection(flags, layoutDirection);
-        }
-        public override void OnChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, bool isCurrentlyActive)
-        {
-
-            View fg = ((recyclarAdapter.ViewHolder)viewHolder).fg;
-            DefaultUIUtil.OnDraw(c, recyclerView, fg, dX, dY, actionState, isCurrentlyActive);
-        }
-
-        public override void OnChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, bool isCurrentlyActive)
-        {
-            View fg = ((recyclarAdapter.ViewHolder)viewHolder).fg;
-            DefaultUIUtil.OnDrawOver(c, recyclerView, fg, dX, dY, actionState, isCurrentlyActive);
-        }
+        </RelativeLayout>
 
 
 
+    <RelativeLayout
+                android:background="@color/bgrow"
 
-    }
-}
+           android:id="@+id/bg" 
+        android:layout_width="match_parent"
+        android:layout_height="match_parent">
+
+          <ImageView
+       android:layout_width="wrap_content"
+       android:layout_height="wrap_content"
+   android:layout_alignParentRight="true"
+        android:src="@drawable/delete"
+        />
+
+        </RelativeLayout>
+   
+         <RelativeLayout
+           android:id="@+id/fg" 
+        android:background="@android:color/white"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+
+        <TextView
+            android:layout_margin="10dp"
+            android:padding="10dp"
+            android:textSize="15sp"
+       android:layout_width="match_parent"
+       android:layout_height="wrap_content"
+   android:id="@+id/tv" 
+   />
+</RelativeLayout>
+
+</FrameLayout>
